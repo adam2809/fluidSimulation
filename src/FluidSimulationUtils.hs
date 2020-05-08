519930ae4bc9f041020e02447966598a70fb48db
adam2809 <adam.kule@gmail.com>
2020-05-08 14:02:08 +0100

    Wrote basic verison of diffuse

--- src/FluidSimulationUtils.hs
+++ src/FluidSimulationUtils.hs
@@ -9,9 +9,13 @@ import Data.Array.Repa.Shape (shapeOfList)
 import FluidSquare
 import Control.Monad (mapM)
 
--- Tam jest inaczej niż myślałem
-setBoundsX :: (R.Source r Double) => R.Array r R.DIM2 Double -> R.Array R.D R.DIM2 Double
-setBoundsX arr = R.traverse arr id (\src (R.Z R.:. x R.:. y) -> if
+-- TODO The definition below is a placeholder
+setAllBounds :: (R.Source r Double) => R.Array r R.DIM2 Double -> R.Array R.D R.DIM2 Double
+setAllBounds = setXBound
+
+
+setXBound :: (R.Source r Double) => R.Array r R.DIM2 Double -> R.Array R.D R.DIM2 Double
+setXBound arr = R.traverse arr id (\src (R.Z R.:. x R.:. y) -> if
     | x == 0 -> negate $ src (R.Z R.:. 1 R.:. y)
     | x == n-1 -> negate $ src (R.Z R.:. (n-2) R.:. y)
     | otherwise -> 0) where
@@ -27,6 +31,18 @@ else
         cornerIndicies = mapM (const [0,n-1]) [1,2]
 
 
+diffAccuracy = 20
+
+-- TODO
+-- The second parameter of the first argument should be r (with constraint that
+--  r is the array type) instead of R.D but types don't match up otherwise so
+-- leaving this to fix later
+diffuse :: R.Array R.D R.DIM2 Double -> Int -> Double -> R.Array R.D R.DIM2 Double
+diffuse arr dt diff = foldr (\_ accum -> setAllBounds $ diffApproxArgs accum) arr [1..diffAccuracy]
+    where
+        diffApproxArgs = diffApprox arr dt diff
+
+
 diffApprox :: (R.Source r Double) => R.Array r R.DIM2 Double -> Int -> Double -> R.Array r R.DIM2 Double -> R.Array R.D R.DIM2 Double
 diffApprox org dt diff arr = R.traverse arr id (\src i -> let sumNeighs = sum $ map (\j -> arr R.! j) (neighbors (n-1) i) in
     ((org R.! i) + a*sumNeighs)/(1+4*a)) where
@@ -38,6 +54,6 @@ neighbors maxIndex (R.Z R.:. x R.:. y) = map shapeOfList (filter invalid [[x+1,y
     invalid = \[a,b] -> (a >= 0) && (a <= maxIndex) && (b >= 0) && (b <= maxIndex)
 
 
-m=(R.computeP $ setBoundsX $ velocityX testFs) :: IO(R.Array R.U R.DIM2 Double)
+m=(R.computeP $ setXBound $ velocityX testFs) :: IO(R.Array R.U R.DIM2 Double)
 t=(R.computeP $ setCorners $ velocityX testFs) :: IO(R.Array R.U R.DIM2 Double)
-y=(R.computeP $ setCorners $ setBoundsX $ velocityX testFs) :: IO(R.Array R.U R.DIM2 Double)
+y=(R.computeP $ setCorners $ setXBound $ velocityX testFs) :: IO(R.Array R.U R.DIM2 Double)
