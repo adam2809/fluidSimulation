964e0908316673e2abee426ba9105087cb980281
adam2809 <adam.kule@gmail.com>
2020-05-30 19:52:31 +0200

    Started writing streaming server

--- server/Main.hs
+++ server/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
 
+
+import           Text.Blaze ((!))
+import qualified Text.Blaze.Html4.Strict as H
+import qualified Text.Blaze.Html4.Strict.Attributes as A
+
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello, World!"
+main = simpleHTTP nullConf $ ok $ toResponse $ appTemplate (H.h1 "hello, blaze!")
+
+appTemplate :: H.Html -> H.Html
+appTemplate body =
+    H.html $ do
+        H.head $ do
+            H.title (H.toHtml ("Fluid simulation" :: String))
+            H.meta ! A.httpEquiv "Content-Type" ! A.content "text/html;charset=utf-8"
+            H.meta ! A.name "keywords" ! A.content "happstack, blaze, html"
+            H.body $ do
+                body
